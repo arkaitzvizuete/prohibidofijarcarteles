af9bee9e1a793b68984e6f311377cda9f8374499
Arkaitz Vizuete <arkaitz.vizu@gmail.com>
2020-09-26 17:48:55 +0200

    Update dev.js

--- resources/js/dev.js
+++ resources/js/dev.js
@@ -69,7 +69,7 @@ function successCallback(mediaStream) {
                 track.applyConstraints(
                     {
                         advanced: [
-                            {zoom: capabilities.zoom.max}
+                            {whiteBalanceMode: "manual"}
                         ]
                     }
                 );
